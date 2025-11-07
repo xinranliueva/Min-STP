98e9f45d2d7d585b615a58b86de938ff0ba47da5
Xinran Liu <xinran.liu@vanderbilt.edu>
2025-11-07 15:24:26 -0600

    fix bug in hardsort

--- softsort.py
+++ softsort.py
@@ -12,7 +12,7 @@ class SoftSort_p2(torch.nn.Module):
         scores: elements to be sorted. Typical shape: batch_size x n
         """
         scores = scores.unsqueeze(-1)
-        sorted = scores.sort(descending=False, dim=1)[0]
+        sorted = scores.sort(descending=True, dim=1)[0]
         pairwise_diff = ((scores.transpose(1, 2) - sorted) ** 2).neg() / self.tau
         P_hat = pairwise_diff.softmax(-1)
 
